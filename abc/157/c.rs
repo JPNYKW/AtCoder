aa1875e4baf237372e1c95abaff3ba241fda0c51
haruto hirakawa <31243896+jpnykw@users.noreply.github.com>
2020-03-11 16:21:24 +0900

    ac

---
+++ abc/157/c.rs
@@ -0,0 +1,69 @@
+// https://atcoder.jp/contests/abc157/submissions/10752735
+
+macro_rules! input {
+  (source = $s:expr, $($r:tt)*) => {
+    let mut iter = $s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+  ($($r:tt)*) => {
+    let mut s = {
+      use std::io::Read;
+      let mut s = String::new();
+      std::io::stdin().read_to_string(&mut s).unwrap();
+      s
+    };
+    let mut iter = s.split_whitespace();
+    input_inner!{iter, $($r)*}
+  };
+}
+
+macro_rules! input_inner {
+  ($iter:expr) => {};
+  ($iter:expr, ) => {};
+
+  ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+    let $var = read_value!($iter, $t);
+    input_inner!{$iter $($r)*}
+  };
+}
+
+macro_rules! read_value {
+  ($iter:expr, ( $($t:tt),* )) => {
+    ( $(read_value!($iter, $t)),* )
+  };
+
+  ($iter:expr, [ $t:tt ; $len:expr ]) => {
+    (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+  };
+
+  ($iter:expr, chars) => {
+    read_value!($iter, String).chars().collect::<Vec<char>>()
+  };
+
+  ($iter:expr, usize1) => {
+    read_value!($iter, usize) - 1
+  };
+
+  ($iter:expr, $t:ty) => {
+    $iter.next().unwrap().parse::<$t>().expect("Parse error")
+  };
+}
+
+fn main() {
+  input! {
+    n: usize, m: usize,
+    sc: [(usize, usize); m]
+  }
+  
+  for x in 0..10i32.pow(n as u32) {
+    let t: String = x.to_string();
+    if
+      t.len() == n &&
+      sc.iter().all(|&(s, c)| t.chars().nth(s - 1) == std::char::from_digit(c as u32, 10)) {
+        println!("{}", x);
+        return;
+    }
+  }
+  
+  println!("-1");
+}
